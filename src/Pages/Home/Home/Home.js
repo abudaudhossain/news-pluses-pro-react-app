b368806d3c0912de1cba35d71f0086c2d7103eeb
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-10-16 10:15:57 +0600

    add newspluses pro react app

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../Shared/Header/Header/Header';
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home</h1>
+           <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Header/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>this hader</h1>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Header;
